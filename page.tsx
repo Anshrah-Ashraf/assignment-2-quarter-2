c0316844b8169f99433b4463c2a3e062476e877f
Anshrah Ashraf <159923970+Anshrah-Ashraf@users.noreply.github.com>
2024-10-02 13:43:43 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+export default function Home(){
+  return(
+    <div className="homeContainer">
+      <div className="childContainer">
+        Hey!
+        <br/>
+        My name is <span className="aqua"> <b>Anshrah</b> </span>
+        <br/>
+        I am a Next JS developer
+      </div>
+    </div>
+  );
+};    
